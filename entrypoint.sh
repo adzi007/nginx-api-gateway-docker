9e9a80d52187b94309b866788214f8422934b6c7
adzi fauzi ahmad <adzi.fauziahmad@gmail.com>
2025-01-08 05:06:31 +0700

    proxy login and logout to keycloack

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replace placeholders in nginx.conf.template with environment variables
-envsubst '${MICROSERVICE_CART_HOST} ${MICROSERVICE_CART_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${MICROSERVICE_AUTH_HOST} ${MICROSERVICE_AUTH_PORT} ${MICROSERVICE_AUTH_REALM} ${MICROSERVICE_CART_HOST} ${MICROSERVICE_CART_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Example multiple microservices variable
 # envsubst '${MICROSERVICE_CART_HOST} ${MICROSERVICE_CART_PORT} ${MICROSERVICE_USERS_HOST} ${MICROSERVICE_USERS_PORT} ${MICROSERVICE_PRODUCTS_HOST} ${MICROSERVICE_PRODUCTS_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
